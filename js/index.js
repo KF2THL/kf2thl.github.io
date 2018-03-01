4e0058a8d1dd0ee3eaa856eab285bca9fb81e8aa
RealPyro <gut_e@live.com>
2018-03-01 21:00:08 +0700

    updated

--- js/index.js
+++ js/index.js
@@ -76,3 +76,17 @@ function drags(dragElement, resizeElement, container) {
     resizeElement.removeClass('resizable');
   });
 }
+
+$('.Show').click(function() {
+    $('#target').show(800);
+    $('.Show').hide(0);
+    $('.Hide').show(0);
+});
+$('.Hide').click(function() {
+    $('#target').hide(800);
+    $('.Show').show(0);
+    $('.Hide').hide(0);
+});
+$('.toggle').click(function() {
+    $('#target').toggle('slow');
+});
